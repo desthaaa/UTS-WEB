206bef0330d3ff36cba90c0dfbd2c10b056c817a
destha <desthaciacia@gmail.com>
2025-02-28 14:57:27 +0700

    berhasil muncul halaman login register

--- todo-app/server.js
+++ todo-app/server.js
@@ -1,34 +1,64 @@
+// const express = require("express");
+// const authRoutes = require("./routes/authRoutes");
+// const taskRoutes = require("./routes/taskRoutes");
+// const path = require("path");
+// require("dotenv").config();
+
+// const app = express();
+// const port = process.env.PORT || 4000;
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true })); 
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "ejs");
+
+// app.use("/auth", authRoutes);
+// app.use("/tasks", taskRoutes);
+
+// app.get("/", (req, res) => {
+//   res.redirect("/login");
+// });
+
+// app.get("/login", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "login.html"));
+// });
+
+// app.get("/register", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "register.html"));
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server running on http://localhost:${port}`);
+// });
+
 const express = require("express");
-const authRoutes = require("./routes/authRoutes");
-const taskRoutes = require("./routes/taskRoutes");
 const path = require("path");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+// Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public"))); //
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-app.use("/auth", authRoutes);
-app.use("/tasks", taskRoutes);
-
+// ke halaman login
 app.get("/", (req, res) => {
-  res.redirect("/login");
+    res.redirect("/login");
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
-
+// login.html dan register.html bisa diakses
 app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "login.html"));
+    res.sendFile(path.join(__dirname, "public", "login.html"));
 });
 
 app.get("/register", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "register.html"));
+    res.sendFile(path.join(__dirname, "public", "register.html"));
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
 });

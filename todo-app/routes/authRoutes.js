a35ff0b2731c828b112963107a6dad00f2f1a24d
destha <desthaciacia@gmail.com>
2025-02-28 15:59:04 +0700

    postman ok

--- todo-app/routes/authRoutes.js
+++ todo-app/routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const authController = require("../controllers/authController");
-const authMiddleware = require("../middlewares/authMiddleware");
+
+// const authMiddleware = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-router.post("/register", authMiddleware, authController.register);
+router.post("/register", authController.register);
 router.post("/login", authController.login);
 
 module.exports = router;
